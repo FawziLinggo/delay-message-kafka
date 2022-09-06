291e5c5e70e2e805b61f4588171d77cb7574af2b
FawziLinggo <fawzilinggo@gmail.com>
2022-09-06 09:01:28 +0700

    add to producer

--- src/main/java/docker/DelayDocker.java
+++ src/main/java/docker/DelayDocker.java
@@ -1,6 +1,9 @@
 package docker;
 
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
 
 import java.io.FileReader;
@@ -13,6 +16,7 @@ public class DelayDocker {
 
 
     private static KafkaConsumer<String, String> consumer;
+    private static KafkaProducer<String, String> producer;
     private static TopicPartition topicPartition;
     private static Properties props = new Properties();
 
@@ -25,15 +29,19 @@ public class DelayDocker {
         // Change Consumer Group to each Product
         Long delay = Long.parseLong((String) props.get("delay.in.ms"));
         String grup_id = "group_of_" + delay /60000 +"_minutes";
+        String TOPIC_NAME_PRODUCER = TOPIC_NAME + "_delay_"+delay /60000 +"_minutes";
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, grup_id);
 
 //        int PARTITION = number_of_partitions-1;
         consumer = new KafkaConsumer<>(props);
+        producer = new KafkaProducer<>(props);
+
 
 
         while (true) {
             for (int i = 0; i < number_of_partitions; i++) {
                 topicPartition = new TopicPartition(TOPIC_NAME, i);
+
                 // Add Commit
                 Map<TopicPartition, OffsetAndMetadata> committed = consumer.committed(new HashSet<>(List.of(topicPartition)));
                 OffsetAndMetadata offsetAndMetadata = committed.get(topicPartition);
@@ -45,20 +53,22 @@ public class DelayDocker {
                     consumer.seek(topicPartition, offsetAndMetadata);
                 }
 
-            int consumerPollDurationOfSeconds = Integer.parseInt((String) props.get("Poll.Duration"));
-            long timestampnow = Instant.now().toEpochMilli();
-
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(consumerPollDurationOfSeconds));
-            for (ConsumerRecord<String, String> record : records) {
-                if ((timestampnow - record.timestamp()) >= delay) {
-                    System.out.printf("consumed: timestamp %s, key = %s, value = %s, partition id= %s, offset = %s%n",
-                            record.timestamp(), record.key(), record.value(), record.partition(), record.offset());
-                    consumer.commitAsync();
-                } else {
-                    consumer.seek(topicPartition, record.offset());
+                int consumerPollDurationOfSeconds = Integer.parseInt((String) props.get("Poll.Duration"));
+                long timestampnow = Instant.now().toEpochMilli();
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(consumerPollDurationOfSeconds));
+                try {
+                    for (ConsumerRecord<String, String> record : records) {
+                        if ((timestampnow - record.timestamp()) >= delay) {
+                            producer.send(new ProducerRecord<>(TOPIC_NAME_PRODUCER, record.value()));
+                            consumer.commitAsync();
+                        } else {
+                            consumer.seek(topicPartition, record.offset());
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
             }
-        }
         }
     }
 }
