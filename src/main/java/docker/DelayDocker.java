07362da988c3ab19e5c3907d9c0b5bb155cf9bd5
FawziLinggo <fawzilinggo@gmail.com>
2022-09-06 09:36:44 +0700

    add to file Log

--- src/main/java/docker/DelayDocker.java
+++ src/main/java/docker/DelayDocker.java
@@ -1,11 +1,12 @@
 package docker;
 
+import Props.DelayProperties;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
-
+import org.apache.log4j.Logger;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
@@ -14,41 +15,40 @@ import java.util.*;
 
 public class DelayDocker {
 
-
-    private static KafkaConsumer<String, String> consumer;
-    private static KafkaProducer<String, String> producer;
-    private static TopicPartition topicPartition;
-    private static Properties props = new Properties();
+    final static Logger logger = Logger.getLogger(DelayDocker.class.getName());
+    private static final Properties props = new Properties();
 
     public static void main(String[] args) throws InterruptedException, IOException {
+        ParameterTool params = ParameterTool.fromArgs(args);
+        Properties props = new DelayProperties(params.getRequired("config.delay.path")).build();
 
-        props.load(new FileReader("src/main/resources/consumer-dcoker.properties"));
         String TOPIC_NAME = (String) props.get("topic.consumer.name");
         int number_of_partitions = Integer.parseInt((String) props.get("number.of.partitions"));
 
         // Change Consumer Group to each Product
-        Long delay = Long.parseLong((String) props.get("delay.in.ms"));
+        long delay = Long.parseLong((String) props.get("delay.in.ms"));
         String grup_id = "group_of_" + delay /60000 +"_minutes";
         String TOPIC_NAME_PRODUCER = TOPIC_NAME + "_delay_"+delay /60000 +"_minutes";
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, grup_id);
 
-//        int PARTITION = number_of_partitions-1;
-        consumer = new KafkaConsumer<>(props);
-        producer = new KafkaProducer<>(props);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
 
 
         while (true) {
             for (int i = 0; i < number_of_partitions; i++) {
-                topicPartition = new TopicPartition(TOPIC_NAME, i);
+                TopicPartition topicPartition = new TopicPartition(TOPIC_NAME, i);
 
                 // Add Commit
                 Map<TopicPartition, OffsetAndMetadata> committed = consumer.committed(new HashSet<>(List.of(topicPartition)));
                 OffsetAndMetadata offsetAndMetadata = committed.get(topicPartition);
                 consumer.assign(Collections.singleton(topicPartition));
 
+                // Check Offset and Metadata
                 if (offsetAndMetadata == null) {
                     consumer.seekToBeginning(consumer.assignment());
+                    logger.warn(String.format("Offset and Metadata null ") + offsetAndMetadata);
                 } else {
                     consumer.seek(topicPartition, offsetAndMetadata);
                 }
@@ -61,12 +61,15 @@ public class DelayDocker {
                         if ((timestampnow - record.timestamp()) >= delay) {
                             producer.send(new ProducerRecord<>(TOPIC_NAME_PRODUCER, record.value()));
                             consumer.commitAsync();
+                            logger.info(String.format("successfully sent message. "+ delay /60000
+                                    +" minutes delay. commit(partition : "+ record.partition()
+                                    + ", offset : "+record.offset()+")"));
                         } else {
                             consumer.seek(topicPartition, record.offset());
                         }
                     }
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    logger.error(String.format("Error : %s", e.getMessage()));
                 }
             }
         }
